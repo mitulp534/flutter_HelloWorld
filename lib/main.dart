c0995204fa168588219cfea64b2fb5717cb8971f
mitulp534 <mitulp534@gmail.com>
2018-06-23 18:44:07 +0530

    This is the first project of the flutter. this is very intresting project. After this project i am going to code more in the flutter.

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+    void main(){
+      runApp(new MyApp());
+    }
+
+    class MyApp extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return new MaterialApp(
+          title: "My App",
+          home: new HomePage(),
+        );
+      }
+    }
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+       String mytext = "Hello world";
+
+  void _changeText(){
+    setState(() {
+      if(mytext.startsWith("H")){
+        mytext = "Welcome World";
+      }else{
+        mytext = "Hello World";
+      }
+    });
+  }
+  Widget _bodywidget(){
+    return new Container(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(mytext,style: new TextStyle(
+              fontSize: 22.0
+            ),),
+            new RaisedButton(
+              child: new Text("Click",style: new TextStyle(
+                color: Colors.white
+              ),),
+              onPressed: _changeText ,
+              color: Colors.red,
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home Page"),
+      ),
+      body: _bodywidget()
+    );
+  }
+}
+
